7340e723991838de0780c55e70b95598fe175d7f
andresalasia <andresmalasia@gmail.com>
2023-11-20 20:25:19 -0300

    comienzo con lo necesario para hacer la librería instalable

---
+++ setup.py
@@ -0,0 +1,18 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, "requirements.txt")
+
+def get_requirements():
+    with codecs.open(REQUIREMENTS_FILE) as buff:
+        return buff.read().splitlines()
+    
+setup(
+    name="src",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    version="0.1.0",
+)
